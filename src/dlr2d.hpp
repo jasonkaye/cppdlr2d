aebb7381efb6255a56d9ed10e6cf975d5492524e
Jason Kaye <jkaye@flatironinstitute.org>
2025-12-20 15:07:25 -0500

    remove obselete functions

--- src/dlr2d.hpp
+++ src/dlr2d.hpp
@@ -115,75 +115,6 @@ void build_dlr2d_if_3term(double lambda, double eps, std::string path,
 nda::array<int, 2> build_dlr2d_if_3term(double lambda, double eps);
 
 /*!
- * \brief Obtain 2D DLR Matsubara frequency grid and compressed 2D DLR real
- * frequency grid
- *
- * This function generates an HDF5 file in the specified path containing the
- * 2D DLR Matsubara frequency grid points in terms of Matsubara frequency index
- * pairs and the 2D DLR real frequency grid points in terms of 1D DLR real
- * frequency grid index pairs.
- *
- * To generate the 2D DLR Matsubara frequency grid, it uses the same method as
- * in \ref build_dlr2d_if. It then compresses the 2D DLR real frequency grid
- * using the pivoted Gram-Schmidt algorithm on the columns of the row-compressed
- * kernel matrix.
- *
- * \param[in] lambda      DLR cutoff parameter
- * \param[in] eps         Error tolerance
- * \param[in] path        Path to directory in which to save 2D DLR Mat. freqs.
- * \param[in] filename    Name of file in which to save 2D DLR Mat. freqs.
- *
- * \note For a fermionic Matsubara frequency i*nu_n = (2n+1)*pi/beta, we refer
- * to n as its index. An index pair (m, n) corresponds to the 2D Matsubara
- * frequency point (i nu_m, i nu_n).
- *
- * \note THIS FUNCTION IS OLD AND MAY NOT WORK PROPERLY. TEST BEFORE USING.
- *
- * \note This is not the method proposed in Kiese et al., "Discrete Lehmann
- * representation of three-point functions", arXiv:2405.06716, which does not
- * recompress the 2D DLR real frequency grid. That method yields an
- * "overcomplete" representation, whereas this method yields a fully compressed
- * representation.
- */
-void build_dlr2d_ifrf(double lambda, double eps, std::string path,
-                      std::string filename);
-
-std::pair<nda::array<int, 2>, nda::array<int, 2>>
-build_dlr2d_ifrf(double lambda, double eps);
-
-/*!
- * \brief Obtain 2D DLR Matsubara frequency grid, using all Matsubara
- * frequencies up to specified cutoff as fine grid
- *
- * This function generates an HDF5 file in the specified path containing the
- * 2D DLR Matsubara frequency grid points in terms of Matsubara frequency index
- * pairs.
- *
- * \param[in] lambda      DLR cutoff parameter
- * \param[in] niom_dense  # Matsubara frequencies per dimension in fine grid
- * \param[in] eps         Error tolerance
- * \param[in] path        Path to directory in which to save 2D DLR Mat. freqs.
- * \param[in] filename    Name of file in which to save 2D DLR Mat. freqs.
- *
- * \note For a fermionic Matsubara frequency i*nu_n = (2n+1)*pi/beta, we refer
- * to n as its index. An index pair (m, n) corresponds to the 2D Matsubara
- * frequency point (i nu_m, i nu_n).
- *
- * \note THIS FUNCTION IS OLD AND MAY NOT WORK PROPERLY. TEST BEFORE USING.
- *
- * \note This is not the standard way of generating the 2D DLR grid, and will be
- * slow for large values of the DLR cutoff Lambda and tolerance epsilon. The
- * function build_dlr2d_if is the standard, more efficient approach, and
- * corresponds to the method proposed in Kiese et al., "Discrete Lehmann
- * representation of three-point functions", arXiv:2405.06716.
- */
-void build_dlr2d_if_fullgrid(double lambda, int niom_dense, double eps,
-                             std::string path, std::string filename);
-
-nda::array<int, 2> build_dlr2d_if_fullgrid(double lambda, int niom_dense,
-                                           double eps);
-
-/*!
  * \brief Read 2D DLR Matsubara frequency grid from file
  *
  * This functions reads the 2D DLR Matsubara frequency grid from an HDF5 file
@@ -266,42 +197,8 @@ fmatrix build_cf2if_3term(double beta, nda::vector<double> dlr_rf,
                           nda::array<int, 2> dlr2d_if);
 
 /*!
- * \brief Build matrix which maps coefficients of a 2D DLR expansion to its
- * values on the 2D DLR imaginary (Matsubara) frequency grid, using a
- * recompressed 2D DLR
- *
- * This function differs from \ref build_cf2if in that it uses a DLR
- * with recompressed 2D real frequency pairs; see \ref build_dlr2d_ifrf and \ref
- * read_dlr2d_rfif.
- *
- * \param[in] beta          Inverse temperature
- * \param[in] dlr_rf        1D DLR real frequencies
- * \param[in] dlr2d_rfidx   Compressed 2D DLR real frequency index pairs
- * \param[in] dlr2d_if      2D DLR Matsubara frequency grid
- *
- * \return Coefficients to values matrix
- */
-fmatrix build_cf2if_square(double beta, nda::vector<double> dlr_rf,
-                           nda::array<int, 2> dlr2d_rfidx,
-                           nda::array<int, 2> dlr2d_if);
-
-/*!
- * \brief Transform values of a 2D DLR expansion on the 2D DLR imaginary
- * (Matsubara) frequency grid to its coefficients
- *
- * \param[in] cf2if  Coefficients to values matrix
- * \param[in] vals   Values of 2D DLR expansion on 2D DLR Mat. freq. grid
- * \param[in] r      # basis functions in 1D DLR
- *
- * \return Coefficients of 2D DLR expansion
- *
- * \note The matrix \p cf2if should be obtained using \ref build_cf2if.
- */
-std::tuple<nda::array<dcomplex, 3>, nda::array<dcomplex, 1>>
-vals2coefs_if(fmatrix cf2if, nda::vector_const_view<dcomplex> vals, int r);
-
-/*!
- * \brief Transform imaginary frequency values of a 2D DLR expansion to coefficients
+ * \brief Transform imaginary frequency values of a 2D DLR expansion to
+ * coefficients
  *
  * \param[in] r         # basis functions in 1D DLR
  * \param[in] cf2if     Coefficients to values matrix
@@ -317,7 +214,8 @@ vals2coefs(int r, fmatrix cf2if, nda::vector_const_view<dcomplex> vals,
            nda::array_const_view<int, 2> dlr2d_rf);
 
 /*!
- * \brief Transform imaginary frequency values of a 2D DLR expansion to coefficients, multiple expansions
+ * \brief Transform imaginary frequency values of a 2D DLR expansion to
+ * coefficients, multiple expansions
  *
  * \param[in] r         # basis functions in 1D DLR
  * \param[in] cf2if     Coefficients to values matrix
@@ -332,26 +230,6 @@ std::tuple<nda::array<dcomplex, 4>, nda::array<dcomplex, 2>>
 vals2coefs_many(int r, fmatrix cf2if,
                 nda::array_const_view<dcomplex, 2, nda::F_layout> vals,
                 nda::array_const_view<int, 2> dlr2d_rf);
-
-/*!
- * \brief Transform values of multiple 2D DLR expansions on the 2D DLR imaginary
- * (Matsubara) frequency grid to their coefficients
- *
- * \param[in] cf2if  Coefficients to values matrix
- * \param[in] vals   Values of 2D DLR expansions on 2D DLR Mat. freq. grid
- * \param[in] r      # basis functions in 1D DLR
- *
- * \return Coefficients of 2D DLR expansions
- *
- * \note The matrix \p cf2if should be obtained using \ref build_cf2if.
- *
- * \note TODO: Replace \ref vals2coefs_if with a properly templated version of
- * this function.
- */
-std::tuple<nda::array<dcomplex, 4>, nda::array<dcomplex, 2>>
-vals2coefs_if_many(fmatrix cf2if,
-                   nda::array_const_view<dcomplex, 2, nda::F_layout> vals,
-                   int r);
 
 /*!
  * \brief Transform values of a 2D DLR expansion on the 2D DLR imaginary
@@ -393,24 +271,6 @@ std::tuple<nda::array<dcomplex, 4>, nda::array<dcomplex, 2>>
 vals2coefs_if_many_3term(fmatrix cf2if,
                          nda::array_const_view<dcomplex, 2, nda::F_layout> vals,
                          int r);
-
-/*!
- * \brief Transform values of a 2D DLR expansion on the 2D DLR imaginary
- * (Matsubara) frequency grid to its coefficients, using a recompressed 2D DLR
- *
- * This function differs from \ref vals2coefs_if in that it uses a DLR
- * with recompressed 2D real frequency pairs; see \ref build_dlr2d_ifrf and \ref
- * read_dlr2d_rfif.
- *
- * \param[in] cf2if  Coefficients to values matrix
- * \param[in] vals   Values of 2D DLR expansion on 2D DLR Mat. freq. grid
- *
- * \return Coefficients of 2D DLR expansion
- *
- * \note The matrix \p cf2if should be obtained using \ref build_cf2if_square.
- */
-nda::array<dcomplex, 1>
-vals2coefs_if_square(fmatrix cf2if, nda::vector_const_view<dcomplex> vals);
 
 /*!
  * \brief Evaluate a 2D DLR expansion at a given fermionic/fermionic Matsubara
@@ -460,25 +320,5 @@ coefs2eval_if_3term(double beta, nda::vector<double> dlr_rf,
                     nda::array_const_view<dcomplex, 3> gc_reg,
                     nda::array_const_view<dcomplex, 1> gc_sng, int m, int n,
                     int channel);
-
-/*!
- * \brief Convert compressed 2D DLR expansion coefficients to ordinary
- * (overcomplete) 2D DLR expansion coefficient storage format
- *
- * This function must be called before using \ref coefs2eval_if to evaluate a
- * compressed DLR expansion with DLR frequency pairs obtained using \ref
- * build_dlr2d_ifrf. It returns 2D DLR expansion coefficients with zeros
- * corresponding to DLR frequency pairs which were not selected during the
- * compression.
- *
- * \param[in] r             # basis functions in 1D DLR
- * \param[in] dlr2d_rfidx   Compressed 2D DLR real frequency index pairs
- * \param[in] gc            Compressed 2D DLR expansion coefficients
- *
- * \return 2D DLR regular and singular expansion coefficients
- */
-std::tuple<nda::array<dcomplex, 3>, nda::array<dcomplex, 1>>
-uncompress_basis(int r, nda::array<int, 2> dlr2d_rfidx,
-                 nda::array<dcomplex, 1> gc);
 
 } // namespace dlr2d
